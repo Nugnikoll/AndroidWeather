478e64ee6d2da5d7a9e486ce4f6058393fc4334e
Rick <1300012743@pku.edu.cn>
2018-09-29 17:01:58 +0800

    Create a weather project.

---
+++ main/java/nugnikoll/weather/MainActivity.java
@@ -0,0 +1,12 @@
+package nugnikoll.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.weather_info);
+    }
+}
