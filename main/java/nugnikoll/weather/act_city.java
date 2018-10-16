dc1076798887fb5e8ea709be0876267338df1258
Rick <1300012743@pku.edu.cn>
2018-10-16 20:16:32 +0800

    Use the button to go back to MainActivity.

--- main/java/nugnikoll/weather/act_city.java
+++ main/java/nugnikoll/weather/act_city.java
@@ -2,11 +2,29 @@ package nugnikoll.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class act_city extends Activity{
+public class act_city extends Activity implements View.OnClickListener{
+	private ImageView button_back;
+
 	@Override
 	protected void onCreate(Bundle save_instance_state){
 		super.onCreate(save_instance_state);
 		setContentView(R.layout.layout_city);
+
+		button_back = (ImageView) findViewById(R.id.title_back);
+		button_back.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view){
+		switch(view.getId()){
+		case R.id.title_back:
+			finish();
+			break;
+		default:
+			break;
+		}
 	}
 }
